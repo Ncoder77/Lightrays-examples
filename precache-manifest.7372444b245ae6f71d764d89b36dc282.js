356c38b56f7263b0f9fe9a87c9d326e303b02cc5
NCoder77 <niho24@outlook.de>
2022-07-23 23:20:06 +0200

    Updates

---
+++ precache-manifest.7372444b245ae6f71d764d89b36dc282.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3da4e1dea0284b8a3cfc",
+    "url": "/Lightrays/static/css/main.1dc1500d.chunk.css"
+  },
+  {
+    "revision": "3da4e1dea0284b8a3cfc",
+    "url": "/Lightrays/static/js/main.3da4e1de.chunk.js"
+  },
+  {
+    "revision": "b9ed88845cdbbe278d55",
+    "url": "/Lightrays/static/js/1.b9ed8884.chunk.js"
+  },
+  {
+    "revision": "8c63bd77958263770397",
+    "url": "/Lightrays/static/js/2.8c63bd77.chunk.js"
+  },
+  {
+    "revision": "a1712aaaf677cc99437b",
+    "url": "/Lightrays/static/js/runtime~main.a1712aaa.js"
+  },
+  {
+    "revision": "c3718d30cf0b1d719cc890a4e20810b7",
+    "url": "/Lightrays/static/media/logo.c3718d30.png"
+  },
+  {
+    "revision": "9c287fdf4472e9ac923e5030a6eb9e9e",
+    "url": "/Lightrays/index.html"
+  }
+];
